339346eefa0a3a25994a9ed25f5fa0ec2c4b8bbb
ankit singh <as3824115@gmai.com>
2023-02-22 17:22:43 +0530

    js fundammental part-1

---
+++ scripts/scripts-1.js
@@ -0,0 +1,41 @@
+
+// let job1="reactjs developer";
+// let job2="teacher";
+
+/////////////////////////////////////////////////////////////////
+// let previosJob="reactjs developer";
+// let cureentJob="teacher";
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////
+    /* Declaration of variable
+    1.var (badPractice)
+    2.let (good if you know that the variable name would be change)
+    3 const (good to have)
+    */ 
+   /////////////////////////////////////////////////////////
+   /* operators
+   1.+
+   2.-
+   3.*
+   4./
+   5.2**3
+   6.x=x+10; x*=10;
+   7.x=x-10;
+   */
+///////////////////////////////////////////////////////////////////////////Lec-2(String-Template)///////////////////////////////
+const team="IND";
+const over=10;
+console.log(team+" win the match by " +over+ " over");
+console.log(10,"10");
+console.log(` ${team} win the match by ${over} over`);
+console.log(`hi how are you ?,
+ what are you doing`)

28f448d72afb7909473e3cd764ff637b95919b91
ankit singh <as3824115@gmai.com>
2023-02-22 18:38:29 +0530

    type coecion

--- scripts/scripts-1.js
+++ scripts/scripts-1.js
@@ -32,10 +32,43 @@
    7.x=x-10;
    */
 ///////////////////////////////////////////////////////////////////////////Lec-2(String-Template)///////////////////////////////
-const team="IND";
-const over=10;
-console.log(team+" win the match by " +over+ " over");
-console.log(10,"10");
-console.log(` ${team} win the match by ${over} over`);
-console.log(`hi how are you ?,
- what are you doing`)
+// const team="IND";
+// const over=10;
+// console.log(team+" win the match by " +over+ " over");
+// console.log(10,"10");
+// console.log(` ${team} win the match by ${over} over`);
+// console.log(` hi i'am ankit`)
+///////////////////////////////////////////////////////////////////////////////////////////////lec-3(if-else)///////////////////////
+////Type conversion//////
+/*
+const age =2;
+
+const isEligibleForDriviving=18<=age;
+console.log(isEligibleForDriviving)
+
+if(18<=age) {
+    console.log(`as your age is ${age} you are applicable for driving`);
+}
+else{
+    console.log("you are too young for driving 🤔");
+}
+const variable ="kjhkj";
+console.log(typeof variable,variable);
+console.log(typeof Boolean(variable),Boolean(variable));
+
+*/
+////Type coecion//////
+/*
+const value=54;
+const value2="54";
+if(value==value2){
+    console.log("they both are same");
+}
+else{
+    console.log("they both are not same");
+
+}
+
+console.log(value-value2-false-undefined)
+*/
+/////////////////truthy & Falsy Value//////////////////////////////
